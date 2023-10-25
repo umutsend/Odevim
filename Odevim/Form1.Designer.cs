e9925181773e67cc6d96e67fbe60917409cf8538
Umut Şen <54849795+umutsend@users.noreply.github.com>
2023-10-25 22:07:42 +0300

    yeni kodlar

--- Odevim/Form1.Designer.cs
+++ Odevim/Form1.Designer.cs
@@ -28,12 +28,79 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			listBoxHarfler = new System.Windows.Forms.ListBox();
+			buttonYeniOyun = new System.Windows.Forms.Button();
+			textBoxTahmin = new System.Windows.Forms.TextBox();
+			buttonTahmin = new System.Windows.Forms.Button();
+			SuspendLayout();
+			// 
+			// listBoxHarfler
+			// 
+			listBoxHarfler.FormattingEnabled = true;
+			listBoxHarfler.ItemHeight = 20;
+			listBoxHarfler.Location = new System.Drawing.Point(26, 12);
+			listBoxHarfler.Name = "listBoxHarfler";
+			listBoxHarfler.Size = new System.Drawing.Size(150, 384);
+			listBoxHarfler.TabIndex = 0;
+			// 
+			// buttonYeniOyun
+			// 
+			buttonYeniOyun.BackColor = System.Drawing.Color.Lime;
+			buttonYeniOyun.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			buttonYeniOyun.ForeColor = System.Drawing.Color.Black;
+			buttonYeniOyun.Location = new System.Drawing.Point(199, 12);
+			buttonYeniOyun.Name = "buttonYeniOyun";
+			buttonYeniOyun.Size = new System.Drawing.Size(182, 92);
+			buttonYeniOyun.TabIndex = 1;
+			buttonYeniOyun.Text = "Yeni Oyun";
+			buttonYeniOyun.UseVisualStyleBackColor = false;
+			buttonYeniOyun.Click += buttonYeniOyun_Click;
+			// 
+			// textBoxTahmin
+			// 
+			textBoxTahmin.BackColor = System.Drawing.SystemColors.Highlight;
+			textBoxTahmin.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			textBoxTahmin.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+			textBoxTahmin.Location = new System.Drawing.Point(199, 125);
+			textBoxTahmin.Name = "textBoxTahmin";
+			textBoxTahmin.Size = new System.Drawing.Size(182, 38);
+			textBoxTahmin.TabIndex = 2;
+			// 
+			// buttonTahmin
+			// 
+			buttonTahmin.BackColor = System.Drawing.Color.FromArgb(255, 128, 0);
+			buttonTahmin.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			buttonTahmin.ForeColor = System.Drawing.Color.Black;
+			buttonTahmin.Location = new System.Drawing.Point(199, 181);
+			buttonTahmin.Name = "buttonTahmin";
+			buttonTahmin.Size = new System.Drawing.Size(182, 92);
+			buttonTahmin.TabIndex = 1;
+			buttonTahmin.Text = "Tahmin Et";
+			buttonTahmin.UseVisualStyleBackColor = false;
+			buttonTahmin.Click += buttonTahmin_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			BackColor = System.Drawing.Color.FromArgb(255, 192, 192);
+			ClientSize = new System.Drawing.Size(800, 450);
+			Controls.Add(textBoxTahmin);
+			Controls.Add(buttonTahmin);
+			Controls.Add(buttonYeniOyun);
+			Controls.Add(listBoxHarfler);
+			Name = "Form1";
+			Text = "Form1";
+			Load += Form1_Load;
+			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ListBox listBoxHarfler;
+		private System.Windows.Forms.Button buttonYeniOyun;
+		private System.Windows.Forms.TextBox textBoxTahmin;
+		private System.Windows.Forms.Button buttonTahmin;
 	}
 }
--- Odevim/Form1.cs
+++ Odevim/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Odevim
 {
@@ -16,6 +17,60 @@ namespace Odevim
 		{
 			InitializeComponent();
 		}
+		Random random = new Random();
+		string secilenArac;
 
+
+		private void buttonYeniOyun_Click(object sender, EventArgs e)
+		{
+			string[] kelimeler = { "volkswagen", "audi", "bmw", "renault", "dacia", "skodia", "fiat", "maserati", "lamborghini", "ferrari", "ford", "citroen", "honda", "hyundai", "peugeot", "opel", "nissan", "mercedes", "toyota", "volvo", "mazda", "alfa romeo", "aston martin", "bentley" };
+			secilenArac = kelimeler[random.Next(0, 24)];
+			buttonTahmin.Enabled = true;
+			textBoxTahmin.Enabled = true;
+			textBoxTahmin.Text = "";
+			listBoxHarfler.Items.Clear();
+
+		}
+
+		private void buttonTahmin_Click(object sender, EventArgs e)
+		{
+			string yazilan = textBoxTahmin.Text;
+			char[] yazilanharfler = yazilan.ToCharArray();
+			string secilenharfler;
+			if (yazilan == secilenArac)
+			{
+				MessageBox.Show("Tebrikler buldunuz.");
+			}
+			else
+			{
+				foreach (char item in yazilan)
+				{
+					string mesaj = "Harf bulunamadı";
+					if (secilenArac.Contains(item) && !secilenharfler.Contains(item))
+					{
+						secilenharfler += $"{item},";
+					}
+					else if (!secilenArac.Contains(item))
+					{
+
+						listBoxHarfler.Items.Add(mesaj);
+					}
+					textBoxTahmin.Text = "";
+				}
+				if (secilenharfler != "")
+				{
+					secilenharfler = secilenharfler.TrimEnd(',');
+				}
+				listBoxHarfler.Items.Add(secilenharfler);
+
+
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			textBoxTahmin.Enabled=false;
+			buttonTahmin.Enabled = false;
+		}
 	}
 }
